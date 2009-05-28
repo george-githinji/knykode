53f389a591093af6fd6b6256e49bdacc3f17f792
george <georgkam@gmail.com>
2009-05-28 17:15:17 +0300

    added methods to postcode class

--- lib/postcode.rb
+++ lib/postcode.rb
@@ -1,4 +1,20 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 
-puts "Hello World"
+class Postcode
+
+  def code(post_office)
+    
+  end
+
+  def post_office(code)
+    
+  end
+
+  def number_of_postoffices
+
+  end
+
+  def list_all
+    
+  end
+  
+end
