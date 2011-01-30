9ec62c1aab99556dd61a7406d22305a77f71f1d1
George <georgkam@gmail.com>
2011-01-30 17:27:15 +0300

    changes codes to a constant

--- lib/phonecode.rb
+++ lib/phonecode.rb
@@ -4,23 +4,18 @@
 class Phonecode
 #  attr_accessor :operators,:safaricom_prefixes
   #operators
-  @operators = ["Safaricom","Airtel","Yu","Orange"]
+  Operators = ["Safaricom","Airtel","Yu","Orange"]
 
-  @safaricom_prefixes = [0720,0721,0722,0723,0724,0725,0710]
+  Safaricom_prefixes = [0720,0721,0722,0723,0724,0725,0710]
 
-  @airtel_prefixes = [0730,0731,0732,0733,0734,0735,0746]
+  Airtel_prefixes = [0730,0731,0732,0733,0734,0735,0746]
 
-
-  def safaricom_prefixes
-    @safaricom_prefixes
-  end
-  
   def add_safaricom_prefix(prefix)
-     @safaricom_prefixes.push(prefix)
+     Safaricom_prefixes.push(prefix)
   end
 
   def add_airtel_prefix(prefix)
-     @airtel_prefixes.push(prefix)
+     Airtel_prefixes.push(prefix)
   end
 
 end
