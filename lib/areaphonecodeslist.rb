8b1779f4390186594844d7ed4eb1d9e423343116
NjeriChelimo <martha.c.chumo@gmail.com>
2012-08-10 22:50:03 +0300

    updates

---
+++ lib/areaphonecodeslist.rb
@@ -0,0 +1,35 @@
+
+
+# a Hash of diffrent areas and corresponding phone codes
+
+def areaphonecodes
+    {"Nairobi"=>"20", 
+    "Kwale"=>"40",
+    "Voi"=>"43",
+    "Kajiado"=>"23",
+    "Naivasha"=>"50",
+    "Kericho"=>"52"
+    "Kitale"=>"54",
+    "Kakamega"=>"56",
+    "Kisii"=>"58",
+    "Muranga"=>"60",
+    "Nanyuki"=>"62",
+    "Nyahururu"=>"65",
+    "Thika"=>"67",
+    "Marsabit"=>"69",
+    "Mombasa"=>"41",
+    "Malindi"=>"42",
+    "Machakos"=>"44",
+    "Garissa"=>"46",
+    "Nakuru"=>"51",
+    "Eldoret"=>"53",
+    "Bungoma"=>"55",
+    "Kisumu"=>"57",
+    "Homa_Bay"=>"59",
+    "Nyeri"=>"61",
+    "Meru"=>"64",
+    "Karuri"=>"66",
+    "Embu"=>"68",
+    "Wajir"=>"46"
+    }
+end         
--- lib/phonecode.rb
+++ lib/phonecode.rb
@@ -1,14 +1,20 @@
 
-#A class for inetracting with mobile phone providers phone suffixes etc
+#A class for inetracting with phone providers phone suffixes etc
+require 'areaphonecodeslist'
+areaphonecodes
 
 class Phonecode
 #  attr_accessor :operators,:safaricom_prefixes
   #operators
   Operators = ["Safaricom","Airtel","Yu","Orange"]
 
-  Safaricom_prefixes = [0720,0721,0722,0723,0724,0725,0710]
+  Safaricom_prefixes = [0700,0701,0702,0703,0704,0705,0706,0707,0708,0709,0710,0711,0712,0713,0714,0715,0716,0717,0718,0719,0720,0721,0722,0723,0724,0725,0726,0728,0729 ]
 
-  Airtel_prefixes = [0730,0731,0732,0733,0734,0735,0746]
+  Airtel_prefixes = [0730,0731,0732,0733,0734,0735,0736,0737,0738,0739]
+  
+  Yu_prefixes = [0750,0751,0752,0753,0754,0755]
+  
+  Orange_prefixes = [0770,0772,0773,0774,0775]
 
   def add_safaricom_prefix(prefix)
      Safaricom_prefixes.push(prefix)
