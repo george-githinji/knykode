8e658fa4ae376d9611c44e8223c498f9c2298c1d
georgeG <georgkam@gmail.com>
2012-08-11 21:29:11 +0300

    minor formating

--- lib/knykode/postcode.rb
+++ lib/knykode/postcode.rb
@@ -1,19 +1,10 @@
-require 'postcodeslist'
-postcodes
-
 class Postcode
 
-  #returns a postcode value.
-  #Note: Where a postoffice has more than one name, the names are separated by an underscore.
-  #e.g Juja_Road,Kambi_Ya_Moto
-
   def postcode(postoffice)
-    #postoffice.capitalize_first_letter
     check_nil(postcodes[postoffice])
   end
 
   def postoffice(postcode)
-    #probabably not the most efficient way of doing this
     check_nil( postcodes.invert[postcode])
   end
 
