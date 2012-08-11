54b5bbc3a3bc5ecab9d49a233abbcdbc543daa8f
georgeG <georgkam@gmail.com>
2012-08-11 16:27:09 +0300

    align and remove empty line

--- spec/phonecode_spec.rb
+++ spec/phonecode_spec.rb
@@ -1,6 +1,3 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
 require 'phonecode'
 
 describe Phonecode do
@@ -9,7 +6,6 @@ describe Phonecode do
   end
 
   it "should desc" do
-    # TODO
   end
 end
 
--- spec/postcode_spec.rb
+++ spec/postcode_spec.rb
@@ -1,4 +1,3 @@
-
 require 'postcode'
 
 describe Postcode do
