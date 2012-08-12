7aed4c79fec45a4108bd11f09cafe7faa6ab9f37
georgeG <georgkam@gmail.com>
2012-08-12 14:08:16 +0300

    add mobile.rb

--- lib/knykode.rb
+++ lib/knykode.rb
@@ -1,4 +1,17 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'knykode','postcode')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'knykode','mobile')
 
-#p = Knykode::Postcode.new
-#puts p.postcode("Kerugoya")
+# working with the postcode class
+#posta = Knykode::Postcode.new
+
+#get a postcode given a post office name
+#puts posta.postcode("Kerugoya") #10300
+
+#mobile = Knykode::Mobile.new
+
+#list an array of available carriers
+#puts mobile.carriers #["Safaricom", "Airtel", "Yu", "Orange", "Telkom"]
+
+#list prefix codes for safaricom
+#safaricom = Knykode::Mobile::Safaricom.new
+#puts safaricom.prefixes
