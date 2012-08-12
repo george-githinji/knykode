1750950d3a82379580d0b7a70ec75855938f13fd
georgeG <georgkam@gmail.com>
2012-08-12 22:13:33 +0300

    remove pointless tests

--- spec/postcode_spec.rb
+++ spec/postcode_spec.rb
@@ -1,21 +1,9 @@
-require 'postcode'
+require 'knykode/postcode'
 
 describe Postcode do
   before(:each) do
-    @postcode = Postcode.new
+    @postcode = Knykode::Postcode.new
   end
-
-  it "should respond to a postcode method" do
-   @postcode.should respond_to(:postcode)
-  end
-
-  it "should respond to a postoffice method" do
-    @postcode.should respond_to(:postoffice)
-  end
-  
-   it "gives the total number of postoffices" do
-     @postcode.should respond_to(:number_of_postoffices)
-   end
-
+ 
 end
 
