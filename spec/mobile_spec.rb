d99aa013893ad59accd951826f2636a11b0365ed
georgeG <georgkam@gmail.com>
2012-08-13 19:09:00 +0300

    commented example code

---
+++ spec/mobile_spec.rb
@@ -0,0 +1,14 @@
+#mobile = Knykode::Mobile.new
+
+#list an array of available carriers
+#puts mobile.carriers #["Safaricom", "Airtel", "Yu", "Orange", "Telkom"]
+
+#list prefix codes for safaricom
+#puts mobile.safaricom_prefixes
+
+#given a number detect the carrier
+#puts mobile.detect_carrier('0733######')
+
+#get a list of counties
+#counties = Knykode::Counties.new
+#puts counties.names #an array of county names
--- spec/postcode_spec.rb
+++ spec/postcode_spec.rb
@@ -7,3 +7,13 @@ describe Postcode do
  
 end
 
+# working with the postcode class
+#posta = Knykode::Postcode.new
+
+#get a postcode given a post office name
+#puts posta.postcode("Kerugoya") #10300
+#puts posta.postcode("Tom Mboya St") #00400
+#puts posta.postcode("Ol Butyo") #20229
+
+#get postoffice name given a postcode
+#puts posta.postoffice('20229') #Ol Butyo
