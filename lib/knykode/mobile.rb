f902a56b4f4acfc4ff2109fd0b6dc3e75d52cca2
georgeG <georgkam@gmail.com>
2012-08-13 16:49:01 +0300

    refactor, add carrier detect method

--- lib/knykode/mobile.rb
+++ lib/knykode/mobile.rb
@@ -6,36 +6,51 @@ module Knykode
       ['Safaricom','Airtel','Yu','Orange','Telkom']
     end
 
-    class Safaricom
-      def prefixes
-        ['0700','0701','0702','0703','0704','0705','0706','0707','0708','0709','0710',
-         '0711','0712','0713','0714','0715','0716','0717','0718','0719','0720','0721',
-         '0722','0723','0724','0725','0726','0728','0729']	
-      end
-    end
+    #given a phone number determine the carrier
+    def detect_carrier(number)
+      prefix = number.to_s[0,4] unless number.start_with?('+')
 
-    class Airtel
-      def prefixes
-        ['0730','0731','0732','0733','0734','0735','0746','0737','0738','0739']
-      end
-    end
+      case
+      when safaricom_prefixes.include?(prefix)
+        carrier = 'Safaricom'
 
-    class Yu
-      def prefixes
-        [0750,0751,0752,0753,0754,0755]
-      end
-    end
+      when yu_prefixes.include?(prefix)
+        carrier = 'Yu'
 
-    class Orange
-      def prefixes
-        [0770,0772,0773,0774,0775]
-      end
-    end
+      when airtel_prefixes.include?(prefix)
+        carrier = 'Airtel'
 
-    class Telkom
-      def areacode
+      when orange_prefixes.include?(prefix)
+        carrier = 'Orange'
+      else
+        carrier = 'unrecognized format'
 
       end
+      carrier
     end
+
+    def safaricom_prefixes
+      ['0700','0701','0702','0703','0704','0705','0706','0707','0708','0709','0710',
+       '0711','0712','0713','0714','0715','0716','0717','0718','0719','0720','0721',
+       '0722','0723','0724','0725','0726','0728','0729']	
+    end
+
+    def airtel_prefixes
+      ['0730','0731','0732','0733','0734','0735','0746','0737','0738','0739']
+    end
+
+    def yu_prefixes
+      ['0750','0751','0752','0753','0754','0755']
+    end
+
+    def orange_prefixes
+      ['0770','0772','0773','0774','0775']
+    end
+
+    private
+    def prefix_str(arr)
+      arr.join(',')
+    end
+
   end
 end
